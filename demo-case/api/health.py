b724b1b0c9b3620c6b2cd1a99c3422c814522294
vaishnavi718304 <vaishnavi718304@gmail.com>
2026-03-29 15:09:10 -0500

    Add health check handler to health.py

--- demo-case/api/health.py
+++ demo-case/api/health.py
@@ -1,1 +1,6 @@
-
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": '{"ok": true, "message": "Python backend is working"}'
+    }
