f0a1dffd5703d99d00fa7cc958e324cc9e7304e3
vaishnavi718304 <vaishnavi718304@gmail.com>
2026-03-29 18:03:21 -0500

    h

--- demo-case/api/health.py
+++
@@ -1,6 +0,0 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": '{"ok": true, "message": "Python backend is working"}'
-    }
