d1e1debfa136aecb1ee95d5b71a72ff5bb889bf9
vaishnavi718304 <vaishnavi718304@gmail.com>
2026-03-29 20:36:38 -0500

    Change app port to 8080

--- backend/app.py
+++ backend/app.py
@@ -436,5 +436,4 @@ def _cors_preflight():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)
